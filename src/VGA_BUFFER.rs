93dcb0497e93a832f941e92639f628bd2c5a9357
alyx <alexanderbek3@gmail.com>
2022-10-05 18:16:50 +1100

    vga buffer and test function

--- src/VGA_BUFFER.rs
+++ src/VGA_BUFFER.rs
@@ -42,11 +42,11 @@ struct ScreenChar {
 }
 
 const BUFFER_HEIGHT: usize = 25;
-const BUFFER_WIDGTH: usize = 80;
+const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDGTH]; BUFFER_HEIGHT],
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 // writer type for writing ASCII to the screen
@@ -54,4 +54,51 @@ pub struct Writer {
     column_position: usize,      //keeps track of current position in last row
     colour_code: ColourCode,     //colours
     buffer: &'static mut Buffer, // VGA buffer refference, explicit lifetime for whole program
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let colour_code = self.colour_code;
+                self.buffer.chars[row][col] = ScreenChar {
+                    ascii_character: byte,
+                    colour_code,
+                };
+                self.column_position += 1;
+            }
+        }
+    }
+
+    fn new_line(&mut self) {
+        // TODO
+    }
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                //printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // if character not supported print block byte.
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+}
+
+pub fn print_something() {
+    let mut writer = Writer {
+        column_position: 0,
+        colour_code: ColourCode::new(Colour::Green, Colour::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    writer.write_string("Hello World");
 }
--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,6 @@ static HELLO: &[u8] = b"Hello World! Goodbye";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // Cast as a VGA buffer memory address as raw pointer
-    let VGA_BUFFER = 0xb8000 as *mut u8;
-
-    // iterate over bytes of hello string.
-    for (i, &byte) in HELLO.iter().enumerate() {
-        //unsafe block around memory write
-        unsafe {
-            *VGA_BUFFER.offset(i as isize * 2) = byte;
-            *VGA_BUFFER.offset(i as isize * 2 + 1) = 0xD;
-        }
-    }
-
+    VGA_BUFFER::print_something();
     loop {}
 }
