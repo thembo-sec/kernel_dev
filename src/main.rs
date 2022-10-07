fb2974af798eb0f38174c7ebed16617236174c74
alyx <alexanderbek3@gmail.com>
2022-10-07 19:51:28 +1100

    added test harness, removed panic=abort due to test bug

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
 #![no_std]
 #![no_main]
 #![allow(non_snake_case)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
 mod VGA_BUFFER;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // Cast as a VGA buffer memory address as raw pointer
-    println!("Hello Arden, I just implemented my own macro, \nwith a lot of tutorial handholding.");
+    println!("Booting...\nI should put a little spinny guy here to look cool.");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
