9b3350478cddc272445d129971599d33c65cdb99
ash bek <ash.bek@cybercx.com.au>
2024-03-04 09:04:34 +1100

    Add error code

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -62,7 +62,7 @@ extern "x86-interrupt" fn double_fault_handler(
     stack_frame: InterruptStackFrame,
     _error_code: u64,
 ) -> ! {
-    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}\nError Code: {:#?}", stack_frame, _error_code);
 }
 
 /// Exception handler for page faults.
