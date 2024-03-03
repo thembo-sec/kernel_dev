4cba2d889f5ad62b41676a4fa66489295979b3e7
ash bek <ash.bek@cybercx.com.au>
2024-03-04 09:04:26 +1100

    create a dynaic heap allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,18 +1,64 @@
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
+use linked_list_allocator::LockedHeap;
+use x86_64::{
+    structures::paging::{
+        mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
+    },
+    VirtAddr,
+};
+
+use crate::println;
+
+pub const HEAP_START: usize = 0x_4444_4444_0000;
+pub const HEAP_SIZE: usize = 100 * 1024; // 100KiB
 
 #[global_allocator]
-static ALLOCATOR: Dummy = Dummy;
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+/// Initialise the heap memory stack
+///
+/// This function creates the page range, then maps pages to the range.
+pub fn init_heap(
+    mapper: &mut impl Mapper<Size4KiB>,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) -> Result<(), MapToError<Size4KiB>> {
+    println!("Initializing Heap...");
+    let page_range = {
+        let heap_start = VirtAddr::new(HEAP_START as u64);
+        let heap_end = heap_start + HEAP_SIZE - 1u64;
+        let heap_start_page = Page::<Size4KiB>::containing_address(heap_start);
+        let heap_end_page = Page::containing_address(heap_end);
+
+        Page::range_inclusive(heap_start_page, heap_end_page)
+    };
+
+    for page in page_range {
+        let frame = frame_allocator
+            .allocate_frame()
+            .ok_or(MapToError::FrameAllocationFailed)?;
+
+        // allow read/write access to the heap
+        let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+
+        unsafe { mapper.map_to(page, frame, flags, frame_allocator)?.flush() }
+    }
+
+    unsafe {
+        ALLOCATOR.lock().init(HEAP_START as *mut u8, HEAP_SIZE);
+    }
+    println!("[ok]");
+    Ok(())
+}
 
 pub struct Dummy;
 
 unsafe impl GlobalAlloc for Dummy {
-    unsafe fn alloc(&self, _layout: Layout) -> *mut u8{
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
         null_mut()
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
         panic!("Don't call this ya dummy")
-        
     }
 }
