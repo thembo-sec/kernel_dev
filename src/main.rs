ced1d3b51c3171da0ca274be367ae9f2ff5422fb
ash bek <ash.bek@cybercx.com.au>
2024-03-04 09:04:44 +1100

    Add playing with the heap

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,12 @@
 
 extern crate alloc;
 
+use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+use kernel_dev::allocator;
 use kernel_dev::memory::{self, BootInfoFrameAllocator};
 use x86_64::{structures::paging::Page, VirtAddr};
-use alloc::boxed::Box;
 
 mod VGA_BUFFER;
 mod serial;
@@ -22,19 +23,31 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     kernel_dev::init_kernel();
 
-    let alloc = Box::new(41);
-
+    x86_64::instructions::interrupts::enable(); // set sti
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mut mapper = unsafe {
-        memory::init(phys_mem_offset)
-    };
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    let mut frame_allocator = unsafe {
-        BootInfoFrameAllocator::init(&boot_info.memory_map)
-    };
+    // new
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
+    let hv = Box::new(41);
+    println!("Heap value at: {:p}", hv);
+
+    let mut vec = Vec::new();
+    for i in 0..100{
+        vec.push(i);
+    }
+    println!("Vec at {:p}", vec.as_slice());
+
+
+    let ref_c = Rc::new(vec![1,2,3]);
+    let cloned_ref = ref_c.clone();
+    println!("Current reference counted is: {}", Rc::strong_count(&cloned_ref));
+    core::mem::drop(ref_c);
+    println!("Ref dropped, new ref count is: {}", Rc::strong_count(&cloned_ref));
     
-
+    
     #[cfg(test)]
     test_main();
 
