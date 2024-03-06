efc2cea21d3bd4445f60186feb928d5e7f8de9de
ash bek <ash.bek@cybercx.com.au>
2024-03-06 14:06:10 +1100

    add long lived boxes to test

--- tests/heap_allocation.rs
+++ tests/heap_allocation.rs
@@ -64,3 +64,14 @@ fn many_boxes() {
         assert_eq!(*x, i);
     }
 }
+
+#[test_case]
+fn many_boxes_long_lives(){
+    let long_lived = Box::new(41);
+    for i in 0..HEAP_SIZE {
+        let x = Box::new(i);
+        assert_eq!(*x, i);
+    }
+    assert_eq!(*long_lived, 1); // new
+
+}
