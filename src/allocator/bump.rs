b5af24375507b83c8d2d19d7daa52df366db5083
ash bek <ash.bek@cybercx.com.au>
2024-03-06 13:53:51 +1100

    create a bump allocator

---
+++ src/allocator/bump.rs
@@ -0,0 +1,58 @@
+use super::{Locked, align_up};
+use alloc::alloc::{GlobalAlloc, Layout};
+use core::ptr;
+
+/// A simple bump allocator.
+pub struct BumpAllocator {
+    heap_start: usize,
+    heap_end: usize,
+    next: usize,
+    allocations: usize,
+}
+
+impl BumpAllocator {
+    /// Create a new empty bump allocator.
+    pub const fn new() -> Self {
+        BumpAllocator {
+            heap_start: 0,
+            heap_end: 0,
+            next: 0,
+            allocations: 0,
+        }
+    }
+
+    /// Initializes a bump alloactor for a given heap start and heap size
+    ///
+    /// This function is unsafe as it is up to the caller to ensure that the values
+    /// for heap start and heap size are valid.
+    pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
+        self.heap_start = heap_start;
+        self.heap_end = heap_start + heap_size;
+        self.next = heap_start;
+    }
+}
+
+unsafe impl GlobalAlloc for Locked<BumpAllocator> {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let mut bump = self.lock();
+
+        let alloc_start = align_up(bump.next, layout.align());
+        let alloc_end = match alloc_start.checked_add(layout.size())
+        {
+            Some(end) => end,
+            None => return ptr::null_mut(),
+        };
+
+        if alloc_end > bump.heap_end {
+            ptr::null_mut()
+        } else {
+            bump.next = alloc_end;
+            bump.allocations += 1;
+            alloc_start as *mut u8
+        }
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        todo!()
+    }
+}
